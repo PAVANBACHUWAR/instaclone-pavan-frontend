d5fdee84d692bf403293bd6be2878851ed6b0d1f
PAVANBACHUWAR <117831833+PAVANBACHUWAR@users.noreply.github.com>
2022-11-20 18:54:01 +0530

    Add files via upload

---
+++ Instaclone_Frontend/build/precache-manifest.eed7169c1e30657014bfe05bd3350826.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "017976ba9036eda636ca",
+    "url": "/static/css/main.3a220ea2.chunk.css"
+  },
+  {
+    "revision": "017976ba9036eda636ca",
+    "url": "/static/js/main.017976ba.chunk.js"
+  },
+  {
+    "revision": "bfa36157bcc75fb8ea07",
+    "url": "/static/js/1.bfa36157.chunk.js"
+  },
+  {
+    "revision": "7587d2c9267632307ee0",
+    "url": "/static/js/2.7587d2c9.chunk.js"
+  },
+  {
+    "revision": "0da2dd41623d9513095b",
+    "url": "/static/js/runtime~main.0da2dd41.js"
+  },
+  {
+    "revision": "c654d5632bb5e2f7647faabe03331414",
+    "url": "/index.html"
+  }
+];
